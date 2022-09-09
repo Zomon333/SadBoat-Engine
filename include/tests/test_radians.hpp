fae8516565085a09782d09a06697e25fea4344bf
Dagan Poulin <dagan.poulin@mymail.champlain.edu>
2022-09-09 14:14:24 -0400

    Updating file path

--- include/tests/test_radians.hpp
+++ include/tests/test_radians.hpp
@@ -14,7 +14,7 @@ Copyright 2022 Dagan Poulin, Justice Guillory
 #define TEST_RADIANS
 
 #include "../utilities/catch.hpp"
-#include "../primitives/angles.hpp"
+#include "../primitives/angles/radians.hpp"
 
 using namespace std;
 
