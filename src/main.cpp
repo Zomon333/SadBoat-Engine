eed6fe2250bba5485730d7517c709b4d44c9a2b5
Dagan Poulin <dagan.poulin@mymail.champlain.edu>
2022-09-10 10:15:01 -0400

    Expanding on comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,37 @@ Copyright 2022 Dagan Poulin, Justice Guillory
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+
+---------------------------------------------------------------------------------------------------------------------
+SadBoat Engine:
+    The SadBoat Engine is an open-source, free to use game engine being designed in C++.
+    Most of it's functionality is defined in it's header files, making it a mostly header only engine.
+    Consequently, you do not need this main.cpp, specifically, to run your game. It is, however, recommended.
+    Opting to not use this main.cpp file means you may need to reimplement the code found herein. This is up to your discretion.
+
+Disclaimer:
+    We are not responsible for what you do with our code. You can use it as intended, or make your own use of it.
+    If you decide to use it within the parameters we've defined, our documentation exists to help you!
+    If something is not clear in our documentation, you can email contact@sadboat.com and we'll try and clarify.
+    If your usage is outside of the scope of our documentation, we may not be able to provide you any helpful results.
+
+---------------------------------------------------------------------------------------------------------------------
 */
 
 // STD Includes
 //----------------------------------
 #include <iostream>
 
+// Primitive Includes
+//----------------------------------
 #include "../include/primitives/range.hpp"
 #include "../include/primitives/tolerance.hpp"
+#include "../include/primitives/point.hpp"
+
+// Angles
 #include "../include/primitives/angles/angle.hpp"
 #include "../include/primitives/angles/degrees.hpp"
 #include "../include/primitives/angles/radians.hpp"
-#include "../include/primitives/point.hpp"
 
 /*
 ---------------------------------------------------------------------------------------------------------------------
@@ -40,16 +59,29 @@ If CONFIG_TEST is not defined, this will default to CONFIG_PROD
     #define CATCH_CONFIG_RUNNER
     #include "../include/utilities/catch.hpp"
 
+    // Test primitives
+    //----------------------------------
+    #include "../include/tests/test_point.hpp"
     #include "../include/tests/test_range.hpp"
     #include "../include/tests/test_tolerance.hpp"
-    #include "../include/tests/test_radians.hpp"
-    #include "../include/tests/test_point.hpp"
+
+    // Angle tests
     #include "../include/tests/test_degrees.hpp"
+    #include "../include/tests/test_radians.hpp"
+
+
 
 #endif
 
 using namespace std;
 
+//----------------------------------
+//  main(int argc, char* argv[]) function:
+//
+//      Entry point for program. Manages testing runtimes, and launches other session managers.
+//      Return point for all session managers, processes engine shutdown.
+//
+//----------------------------------
 int main(int argc, char* argv[])
 {
     int results = 0;
