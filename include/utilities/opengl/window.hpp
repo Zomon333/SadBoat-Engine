adb4c875dd4b777d6f306119c2d4c7f8d792f5da
Dagan Poulin <dagan.poulin@sadboat.com>
2022-09-21 17:54:56 -0400

    Wrote generic class to open a window.

---
+++ include/utilities/opengl/window.hpp
@@ -0,0 +1,90 @@
+/*
+Copyright 2022 Dagan Poulin, Justice Guillory
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include "../../sb-engine.hpp"
+
+using namespace std;
+
+
+class Window
+{
+    private:
+        GLuint windowID = 0;
+
+        int argc = 0;
+        char* windowName = "";
+
+        int x = 0;
+        int y = 0;
+
+        std::promise<bool> closer;
+        std::future<bool> isFinished;
+
+    public:
+        Window(int x, int y, int* argc, char* name)
+        {
+            this->x = x;
+            this->y = y;
+            this->argc = *argc;
+            this->windowName = name;
+        }
+
+        static void display()
+        {
+            //Do something
+        }
+
+        std::future<bool>* open()
+        {
+            isFinished = closer.get_future();
+
+            Event<int, int> openWindow(
+                [this](int a)
+                {
+                    glutInit(&argc, &windowName);
+                    glutInitDisplayMode(GLUT_RGBA);
+
+                    glutInitWindowSize(x,y);
+                    glutInitContextVersion(4,3);
+                    glutInitContextProfile(GLUT_CORE_PROFILE);
+
+                    windowID = glutCreateWindow(windowName);
+
+                    GLenum glewInitResults = glewInit();
+
+                    glutDisplayFunc(display);
+
+                    this->closer.set_value_at_thread_exit(true);
+                    glutMainLoop();
+                    return 0;
+                }
+            );
+
+            openWindow.launch(0);
+            return &isFinished;
+        }
+
+        void setX(int nX) {x=nX;}
+        void setY(int nY) {y=nY;}
+
+        int getX(){return x;}
+        int getY(){return y;}
+
+        GLuint getWindowID(){return windowID;}
+};
+
+
+
+#endif
