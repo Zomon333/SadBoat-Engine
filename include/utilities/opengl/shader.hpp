d9eb3a4017968aff7dc5f36e8f68546a1d5ebd05
Dagan Poulin <dagan.poulin@sadboat.com>
2022-09-23 19:50:29 -0400

    Implementing some of shader class

--- include/utilities/opengl/shader.hpp
+++ include/utilities/opengl/shader.hpp
@@ -13,16 +13,93 @@ Copyright 2022 Dagan Poulin, Justice Guillory
 #ifndef SHADER_H
 #define SHADER_H
 
-#include <utility>
-#include <cmath>
-
 class Shader
 {
     private:
-
+        GLuint shaderID;
+        GLenum shaderType;
+        
+        const char* code;
 
     public:
-        
+        Shader()
+        {
+            shaderID = glCreateShader(GL_VERTEX_SHADER);
+            this->shaderType = GL_VERTEX_SHADER;
+            this->code = 
+                "#version 430 core"
+                "in vec4 vPosition;"
+                "in vec4 vColor;"
+                ""
+                "out vec4 color;"
+                ""
+                "void"
+                "main()"
+                "{"
+                "   color = vColor;"
+                "}"
+            ;
+            
+        }
+        Shader(GLenum shaderType, string code)
+        {
+            //Allocate a name for our shader sufficient to hold our type of shader
+            shaderID = glCreateShader(shaderType);
+
+            this->shaderType=shaderType;
+            this->code = code.c_str();
+        }
+
+        ~Shader()
+        {
+            if(isShader())
+            {
+                glDeleteShader(shaderID);
+            }
+        }
+
+        int make(bool multiThread=true)
+        {
+            //Get something to catch the result
+            GLint result;
+
+            //Make an event that captures this object and the result by reference
+            Event<GLint,int> makeEvent(
+                [this, &result](int a=0)
+                {
+                    //Cast our data to the format the OpenGL functions want to run with
+                    const GLchar *source = (const GLchar*)code;
+                    //Provide our source code to OpenGL
+                    glShaderSource(shaderID, 1, &source, 0);
+
+                    //Try to compile the shader
+                    glCompileShader(shaderID);
+
+                    glGetShaderiv(shaderID, GL_COMPILE_STATUS, &result);
+
+                    if(result!=GL_TRUE)
+                    {
+                        //There's an issue in shader compilation!
+                        //glGetShaderInfoLog(...)
+                    }
+
+                    //We don't actually need to return the result; we're just *going to*.
+                    return result;
+                }
+            );
+            
+            if(multiThread) {           //If multithreaded
+                makeEvent.launch(0);    //Launch
+                makeEvent.getResult();  //And wait
+            } 
+            else makeEvent(0);          //Else: Just calc
+
+            return result;              //Because result was captured by reference, we don't even care about the returns. It was updated in real time.
+        }
+
+        GLuint getID(){ return this->shaderID; }
+        GLenum getType(){ return this->shaderType; }
+        bool isShader() { return glIsShader(shaderID); }
 };
 
 #endif
