4337ff730b7dbd6c39ec34ec8de349d65bffec15
Dagan Poulin <dagan.poulin@sadboat.com>
2022-10-14 17:57:28 -0400

    Updating makefile to allow vulkan installation

--- install_libs.sh
+++ install_libs.sh
@@ -10,54 +10,27 @@
 #
 
 #
-#       Mesa
+#       Vulkan:
+#           -Mesa3D: Vulkan
+#               -Meson
+#               -Ninja
+#               -GLSLang Tools
+#           -libvulkan-dev
+#           -vulkan-tools
+#           -vulkan-validationlayers-dev
+#           -spirv-tools
 #------------------------------------------------------------------------------
-#   This package contains:
+#   This contains:
 #      -Mesa3D open source graphics library.
+#      -Mesa3D compilation tools
+#      -Development headers for vulkan
+#      -Tools to test your vulkan environment
+#      -Debugging layers for your vulkan envionrment
+#      -Required SPIR-V tools
 #
+make clean
+make vulkan_install
 
-sudo apt-get install mesa                               #Get the most recent Mesa
-sudo apt-get build-dep mesa                             #Update and get it's dependencies
-
-sudo apt install meson					#Meson build configurer
-sudo apt install ninja-build				#Ninja build tool install
-sudo apt install glslang-tools				#glslang dependency
-
-mkdir local-mesa                                        #Make a folder called local-mesa
-cd ./local-mesa                                         #Move into local-mesa
-
-wget https://archive.mesa3d.org//mesa-22.2.0.tar.xz     #Download our version of Mesa
-tar -xf ./mesa-22.2.0.tar.xz                            #Unzip it
-cd ./mesa-22.2.0.tar.xz                                 #Move into out version of Mesa
-
-mkdir build                                             #Make a build directory
-
-meson ./ ./build                                        #Generate ninja build files for that directory
-ninja -C ./build                                        #Build that directory
-sudo ninja -C ./build install                           #Install the built files
-
-cd ../..
-
-
-
-#
-#       libvulkan-dev
-#------------------------------------------------------------------------------
-#   This package contains:
-#      -Development headers for vulkan. 
-#
-sudo apt-get install libvulkan-dev
-
-
-#
-#       vulkan-tools, vulkan-validationlayers-dev, spirv-tools
-#------------------------------------------------------------------------------
-#   This package contains:
-#      -Tools to test your vulkan environment.
-#      -Debugging layers to test your vulkan development.
-#      -Required SPIR-V tools.
-#
-sudo apt install vulkan-tools vulkan-validationlayers-dev spirv-tools
 
 
 
--- makefile
+++ makefile
@@ -1,17 +1,20 @@
 CC = g++
 FLAGS = -std=c++20 -ffreestanding -fpermissive -pthread
 
+BR = "\033[1;36m---------------------------------------------------\033[0m"
+
 DEFINES = -DINCLUDE_VULKAN -DINCLUDE_RAPIDXML
 
 SOURCE_DIR = ./src
 INCLUDE_DIRS = "-I./include/" "-I/usr/include/vulkan/"
 OUT_DIR = ./output
-LIBRARY_DIR = "usr/lib/x86_64-linux-gnu"
+LIBRARY_DIR = "usr/lib/x86_64-linux-gnu" "./lib"
 
 PRODUCTION_RELEASE = -DCONFIG_PROD
 TEST_RELEASE = -DCONFIG_TEST
 
 LIBS = "-lvulkan"
+MESA_V = "mesa-22.2.0"
 
 NAME = "SadBoatEngine-V"
 VERSION = "0.02.7V"
@@ -24,6 +27,61 @@ test:
 	g++ $(FLAGS) $(DEFINES) $(TEST_RELEASE) $(SOURCE_DIR)/*.cpp $(INCLUDE_DIRS) -L$(LIBRARY_DIR) $(LIBS) -o $(OUT_DIR)/$(NAME)$(VERSION)"-T"
 
 clean:
-	rm $(OUT_DIR)/*
+	rm -rf $(OUT_DIR)/* 	&
+	rm -rf ./lib/mesa 		&
 
+vulkan_install:
+	@rm -rf ./lib/mesa &
+	@echo "\033[1;36mInstalling Vulkan Compilation Tools...\033[0m"
+	@echo $(BR);
+
+	sudo apt install meson
+	sudo apt install ninja-build
+	sudo apt install glslang-tools
+	
+	@echo "\n\n"
+	@echo "\033[1;36mDownloading mesa\033[0m"
+	@echo $(BR)
+
+	mkdir ./lib/mesa
+	
+	wget https://archive.mesa3d.org//$(MESA_V).tar.xz
+	mv $(MESA_V).tar.xz ./lib/mesa
+
+	@echo "\n\n"
+	@echo "\033[1;36mUnpacking mesa\033[0m"
+	@echo $(BR)
+	
+	tar --extract -f ./lib/mesa/$(MESA_V).tar.xz 
+	mv -f ./$(MESA_V) ./lib/mesa/
+
+	mkdir ./lib/mesa/$(MESA_V)/build
+	
+	@echo "\n\n"
+	@echo "\033[1;36mCreating ninja config files\033[0m"
+	@echo $(BR)
+
+	meson ./lib/mesa/$(MESA_V) ./lib/mesa/$(MESA_V)/build
+
+	@echo "\n\n"
+	@echo "\033[1;36mCompiling mesa with ninja\033[0m"
+	@echo $(BR)
+
+	ninja -C ./lib/mesa/$(MESA_V)/build
+
+	@echo "\n\n"
+	@echo "\033[1;36mInstalling mesa\033[0m"
+	@echo $(BR)
+
+	sudo apt-get install mesa
+	sudo apt-get build-dep mesa
+
+	sudo ninja -C ./lib/mesa/$(MESA_V)/build install
+
+	@echo "\n\n"
+	@echo "\033[1;36mInstalling libvulkan-dev and vulkan tools\033[0m"
+	@echo $(BR)
+
+	sudo apt-get install libvulkan-dev
+	sudo apt install vulkan-tools vulkan-validationlayers-dev spirv-tools
 
