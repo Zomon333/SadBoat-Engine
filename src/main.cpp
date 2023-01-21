b968667938c1fefea780ff91696c0974670f7ba3
Zomon333 <zomon333@gmail.com>
2023-01-20 19:43:16 -0500

    Assembly changes

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,11 @@ Disclaimer:
 //----------------------------------
 #include "sb-engine.hpp"
 
+#include <immintrin.h>
+
+#include <cpuid.h>
+#include <stdio.h>
+
 using namespace std;
 using namespace SBE;
 
@@ -91,6 +96,218 @@ string testSetup(int argc, char* argv[], string possibleName)
     return name;
 }
 
+struct HardwareAccelerationSettings
+{
+    string CPUName;
+
+    bool MMX;
+    bool SSE;
+    
+    bool SSE2;
+    bool SSE3;
+    bool SSSE3;
+    bool SSE4_1;
+    bool SSE4_2;
+
+    bool AVX;
+    bool AVX512BW;
+    bool AVX512VL;
+    bool AVX512VBMI;
+    bool AVX512_4FMAPS;
+    bool AVX512_4VNNIW;
+    bool AVX512_VPOPCNTDQ;
+    bool AVX512_BITALG;
+    bool AVX512_VBMI2;
+};
+
+bool getHardwareAccelerationSettings(HardwareAccelerationSettings* updatable)
+{
+    // Get CPU Name
+    int vendor[3];
+    
+    __asm__("mov $0, %eax\n\t");
+    __asm__("cpuid\n\t");
+
+    __asm__("mov %%ebx, %0\n\t":"=r" (vendor[0]));
+    __asm__("mov %%edx, %0\n\t":"=r" (vendor[1]));
+    __asm__("mov %%ecx, %0\n\t":"=r" (vendor[2]));
+
+    // Get CPU Func Supported
+    int func=0;
+    __asm__("mov %%eax, %0\n\t":"=r" (func));
+
+    updatable->CPUName=((char*)vendor); 
+    updatable->CPUName.resize(12);
+
+
+    cout<<updatable->CPUName<<endl;
+    cout<<func<<endl;
+
+    int results[4];
+    // Model & Family
+    __asm__("mov $1, %eax\n\t");
+    __asm__("cpuid\n\t");
+    
+    /*
+    EAX BREAKOUT
+    ------------------------------
+    [31:28] Reserved
+    [27:20] Extended Family ID
+    [19:16] Extended Model ID
+    [15:14] Reserved
+    [13:12] Processor Type
+    [11:8]  Family ID
+    [7:4]   Model
+    [3:0]   SteppingID
+    */
+    __asm__("mov %%eax, %0\n\t":"=r" (results[0]));
+    
+    // Additional Features    
+    /*
+    EBX BREAKOUT
+    ------------------------------
+    [24:31] Brand Index
+    [16:23] CLFLUSH Line Size
+    [8:15]  Maximum # of Addressable IDs for logical processors
+    [7:0]   Logical APIC ID
+    */
+    __asm__("mov %%ebx, %0\n\t":"=r" (results[1]));
+    
+    // Feature Flags
+    /*
+    ECX BREAKOUT
+    ------------------------------
+    0   sse3
+    1   pclmulqdq
+    2   dtes64
+    3   monitor
+    4   ds-cpi
+    5   vmx
+    6   smx
+    7   est
+    8   tm2
+    9   ssse3
+    10  cnxt-id
+    11  sdbg
+    12  fma
+    13  cx16
+    14  xtpr
+    15  pdcm
+    16  Reserved
+    17  pcid
+    18  dca
+    19  sse4.1
+    20  sse4.2
+    21  x2apic
+    22  movbe
+    23  popcnt
+    24  tsc-deadline
+    25  aes
+    26  xsave
+    27  osxsave
+    28  avx
+    29  f16c
+    30  rdrnd
+    31  hypervisor
+    */
+    __asm__("mov %%ecx, %0\n\t":"=r" (results[2]));
+    
+    // Feature Flags
+    /*
+    EDX BREAKOUT
+    ------------------------------
+    0   fpu
+    1   vme
+    2   de
+    3   pse
+    4   tsc
+    5   msr
+    6   pae
+    7   mce
+    8   cx8
+    9   apic
+    10  RESERVED
+    11  sep
+    12  mtrr
+    13  pge
+    14  mca
+    15  cmov
+    16  pat
+    17  pse-36
+    18  psn
+    19  clfsh
+    20  RESERVED
+    21  ds
+    22  acpi   
+    23  mmx
+    24  fxsr
+    25  sse
+    26  sse2
+    27  ss
+    28  htt
+    29  tm
+    30  ia64
+    31  pbe
+    */
+    __asm__("mov %%edx, %0\n\t":"=r" (results[3]));
+
+    bool parsed[2][32];
+
+    unsigned int vals[32] = {
+        (0x0001<<0),
+        (0x0001<<1),
+        (0x0001<<2),
+        (0x0001<<3),
+        (0x0001<<4),
+        (0x0001<<5),
+        (0x0001<<6),
+        (0x0001<<7),
+        (0x0001<<8),
+        (0x0001<<9),
+        (0x0001<<10),
+        (0x0001<<11),
+        (0x0001<<12),
+        (0x0001<<13),
+        (0x0001<<14),
+        (0x0001<<15),
+        (0x0001<<16),
+        (0x0001<<17),
+        (0x0001<<18),
+        (0x0001<<19),
+        (0x0001<<20),
+        (0x0001<<21),
+        (0x0001<<22),
+        (0x0001<<23),
+        (0x0001<<24),
+        (0x0001<<25),
+        (0x0001<<26),
+        (0x0001<<27),
+        (0x0001<<28),
+        (0x0001<<29),
+        (0x0001<<30),
+        ((unsigned int)(0x0001<<31)),
+    };
+
+    for(int i=0; i<32; i++)
+    {
+        //cout<<(vals[i] & results[2])<<endl;
+        parsed[0][i]= ((vals[i]&results[2]) > 0);
+        parsed[1][i]= ((vals[i]&results[3]) > 0);
+    }
+
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<32; j++)
+        {
+            cout<<parsed[i][j];
+        }
+        cout<<endl;
+    }
+
+    return true;
+}
+
+
 //----------------------------------
 //  main(int argc, char* argv[]) function:
 //
@@ -114,6 +331,8 @@ int main(int argc, char* argv[])
 
     auto devices = vulkanDevices.getDevices();
     
+    HardwareAccelerationSettings deviceSettings;
+    getHardwareAccelerationSettings(&deviceSettings);
 
 
     return 0;
