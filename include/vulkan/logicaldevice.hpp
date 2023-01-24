b0e5a32c6a235c42e39a3b0af5e66bf3e5973af9
Zomon333 <zomon333@gmail.com>
2023-01-24 17:31:35 -0500

    Moving LogicalDevice init into constructor

--- include/vulkan/logicaldevice.hpp
+++ include/vulkan/logicaldevice.hpp
@@ -34,15 +34,85 @@ namespace SBE
         VkDevice self;
     public:
         // Constructors
+
+        // Make a device, assume some info
+        LogicalDevice(PhysicalDevice* parent)
+        {
+            // We need to initialize our LogicalDevice, so we're setting up some boilerplate structs to store the info for the constructor.
+            VkDeviceCreateInfo initInfo;
+            initInfo.sType=VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+            initInfo.pNext=nullptr;
+            initInfo.flags=0;
+
+
+            // We need some info on the Queues we're initializing inside of it, too.
+            VkDeviceQueueCreateInfo initDevQueue;
+            initDevQueue.sType=VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            initDevQueue.pNext=nullptr;
+            initDevQueue.flags=0;
+
+            // Find some queue family in the collection of queue families.
+            // Ideally, we'd do more work here to find the *right* queue family but for this boilerplate
+            // we're just going to assume the first queue fam is fine.
+
+            // Specifies the family of the queues we want to create
+            // Set to the index in the array of queue families stored in QueueFamilyCollection
+            initDevQueue.queueFamilyIndex=0;
+
+            /*
+            Render categories:
+                -Player
+                -Projectiles
+                -Enemies
+                -Background & UI
+                -Other
+            */
+            initDevQueue.queueCount=5;
+
+            // An optional pointer to an array of floats representing priority of work submitted to each of the queues. These values are normalized.
+            // Use this later for prioritizing rendering for high mobility objects and the player model
+            // Setting this to nullptr has the device treat every queue the same
+            initDevQueue.pQueuePriorities=nullptr;
+            
+            initInfo.queueCreateInfoCount=5;
+            // initInfo.pQueueCreateInfos=
+
+
+
+
+            // initInfo.enabledLayerCount=
+            // initInfo.ppEnabledLayerNames=
+            // initInfo.enabledExtensionCount=
+            // initInfo.ppEnabledExtensionNames=
+            // initInfo.pEnabledFeatures=
+        }
+
+        // Make a device, assume no info
         LogicalDevice(PhysicalDevice* parent, VkDeviceCreateInfo* creationInfo)
         {
             this->host=parent->getHost();
             this->creationInfo=creationInfo;
             vkCreateDevice(parent->getDevice(), creationInfo, host->getAllocationInfo(), &self);
         }
+
         // Mutators
 
+        void update()
+        {
+            parent->update();
+            host=parent->getHost();
+
+        }
+
+        void setParent(auto parent) { this->parent=parent; }
+        void setHost(auto host) { this->host=host; }
+        void setCreationInfo(auto creationInfo) { this->creationInfo=creationInfo; }
+
         // Accessors
+        auto getParent() { return parent; }
+        auto getHost() { return host; }
+        auto getSelf() { return self; }
+        auto getCreationInfo() { return creationInfo; }
     };
 };
 #endif
